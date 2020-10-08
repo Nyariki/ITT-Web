c8edaf4520340f8f27db58617157168e5d347761
Monster <mayorerobert@gmail.com>
2020-10-08 18:19:17 +0300

    Updated migrations

--- db/migrations/scripts/20201007221319_create_events_table.sql
+++ db/migrations/scripts/20201007221319_create_events_table.sql
@@ -1,18 +1,15 @@
 -- // create events table
 -- Migration SQL that makes the change goes here.
 CREATE TABLE events (
-id NUMERIC(20,0) NOT NULL,
-program_type NUMERIC NOT NULL,
+id BIGINT NOT NULL AUTO_INCREMENT,
+type NUMERIC NOT NULL,
 color VARCHAR(25) NOT NULL,
 message  VARCHAR(255) NOT NULL,
-program_time TIMESTAMP NOT NULL,
+time VARCHAR(25) NOT NULL,
 created_at TIMESTAMP DEFAULT NOW(),
-updated_at TIMESTAMP NULL
+updated_at TIMESTAMP NULL,
+CONSTRAINT PK_EVENTS PRIMARY KEY (id)
 );
-
-ALTER TABLE events
-ADD CONSTRAINT PK_EVENTS
-PRIMARY KEY (id);
 
 
 -- //@UNDO
